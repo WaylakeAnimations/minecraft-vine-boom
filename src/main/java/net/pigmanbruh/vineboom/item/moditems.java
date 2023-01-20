238c26361198cfe90b437f9d285aabac272c90f2
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-20 10:35:34 +0700

    Update moditems.java

--- src/main/java/net/pigmanbruh/vineboom/item/moditems.java
+++ src/main/java/net/pigmanbruh/vineboom/item/moditems.java
@@ -25,7 +25,6 @@ public class moditems implements ModInitializer {
     public static final Item VINE_BOOM_ITEM = new Item(new FabricItemSettings());
 
     public void onInitialize() {
-        Registry.register(Registries.ITEM, new Identifier("vineboom", "vine_boom_item"), VINE_BOOM_ITEM);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> entries.add(VINE_BOOM_ITEM));
+        Registry.register(Registries.ITEM, new Identifier("vineboom", "vine_boom_item), VINE_BOOM);
     }
 }
