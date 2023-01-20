655d33f03d4eaa068588159a9d02875c7a523cd9
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-20 08:26:58 +0700

    Update moditems.java

--- src/main/java/net/pigmanbruh/vineboom/item/moditems.java
+++ src/main/java/net/pigmanbruh/vineboom/item/moditems.java
@@ -10,9 +10,9 @@ import net.minecraft.util.Identifier;
 
 public class moditems {
 
-    public static final Item VINE_BOOM = new Item(new FabricItemSettings());
+    public static final Item VINE_BOOM_ITEM = new Item(new FabricItemSettings());
 
     public void onInitialize() {
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "vine_boom"), VINE_BOOM);
+        Registry.register(Registries.ITEM, new Identifier("vineboom", "vine_boom_item"), VINE_BOOM_ITEM);
     }
 }
