964f65b5539c2638b5c733f5f6f9d0944bfa2346
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-20 08:28:21 +0700

    Update mixin.java

--- src/main/java/net/pigmanbruh/vineboom/mixin/mixin.java
+++ src/main/java/net/pigmanbruh/vineboom/mixin/mixin.java
@@ -1,5 +1,6 @@
 package net.pigmanbruh.vineboom.mixin;
 
+import net.pigmanbruh.vineboom.Main;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class mixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		vineboom.LOGGER.info("This line is printed by vineboom's mixin");
+		Main.LOGGER.info("This line is printed by vineboom's mixin");
 	}
 }
