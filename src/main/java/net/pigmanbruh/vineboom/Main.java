88e4b7f753d5d1887ed73ff099ac29577df16d12
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-22 11:50:28 +0700

    Update Main.java

--- src/main/java/net/pigmanbruh/vineboom/Main.java
+++ src/main/java/net/pigmanbruh/vineboom/Main.java
@@ -12,6 +12,6 @@ public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
         Moditems.registerModItems();
-        ModItems.registerItemGroups();
+        Moditems.registerItemGroups();
     }
 }
