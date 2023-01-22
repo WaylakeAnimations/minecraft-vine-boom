44f3adb86862427e2eefafb36a27d999b56207e0
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-22 11:04:46 +0700

    Update Main.java

--- src/main/java/net/pigmanbruh/vineboom/Main.java
+++ src/main/java/net/pigmanbruh/vineboom/Main.java
@@ -1,13 +1,8 @@
 package net.pigmanbruh.vineboom;
 
-import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.pigmanbruh.vineboom.item.Moditems;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 
 public class Main implements ModInitializer {
     public static final String MOD_ID = "vineboom";
