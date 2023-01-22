69ca478c6c8767193373b940d221e3bd63f96535
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-22 10:57:49 +0700

    Update Moditems.java

--- src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
+++ src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
@@ -1,25 +1,14 @@
 package net.pigmanbruh.vineboom.item;
 
-import net.fabricmc.api.ModInitializer;
-import com.google.common.collect.Lists;
-import com.mojang.datafixers.util.Pair;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.pigmanbruh.vineboom.Main;
 import net.minecraft.item.Item;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.util.Identifier;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.entry.RegistryEntry;
-import java.util.List;
-import java.util.Map;
+import net.minecraft.util.Identifier;
 
 public class Moditems {
 
