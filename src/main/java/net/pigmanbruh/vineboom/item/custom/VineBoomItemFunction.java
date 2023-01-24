9a4ad9854ed64d8ef5b99d89c79c381efe5ee7a9
PigmanBruh <pigmanbruh4@gmail.com>
2023-01-24 17:37:01 +0700

    fixing cannot find symbol

--- src/main/java/net/pigmanbruh/vineboom/item/custom/VineBoomItemFunction.java
+++ src/main/java/net/pigmanbruh/vineboom/item/custom/VineBoomItemFunction.java
@@ -33,7 +33,7 @@ public class VineBoomItemFunction extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient() && hand == Hand.MAIN_HAND) {
-            playSoundFromEntity (
+            World.playSoundFromEntity (
                 null,
                 user,
                 VINE_BOOM_SOUND_EVENT,
