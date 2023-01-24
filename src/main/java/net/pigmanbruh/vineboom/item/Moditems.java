04529394f6b4f080cb457ffe8e6395133c89b876
PigmanBruh <pigmanbruh4@gmail.com>
2023-01-24 15:38:26 +0700

    messing with sound

--- src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
+++ src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
@@ -1,7 +1,6 @@
 package net.pigmanbruh.vineboom.item;
 
 import net.pigmanbruh.vineboom.Main;
-import net.pigmanbruh.vineboom.sound.Vineboomsound;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
