8a3f198fc1859f0192b8e744014b630da2ce64d9
PigmanBruh <pigmanbruh4@gmail.com>
2023-01-24 21:57:52 +0700

    trying to fix some stuff

--- src/main/java/net/pigmanbruh/vineboom/item/custom/PlaySound.java
+++ src/main/java/net/pigmanbruh/vineboom/item/custom/PlaySound.java
@@ -8,7 +8,7 @@ import net.minecraft.sound.SoundCategory;
 
 public class PlaySound {
 
-    public void playSound() {
-        World.playSoundFromEntity (null, PlayerEntity, VineBoomItemFunction.VINE_BOOM_SOUND_EVENT, SoundCategory.PLAYERS, 1.0f, 1.0f);
+    public static void playSound(PlayerEntity user) {
+        World.playSoundFromEntity (null, user, VineBoomItemFunction.VINE_BOOM_SOUND_EVENT, SoundCategory.PLAYERS, 1.0f, 1.0f);
     }
 }
