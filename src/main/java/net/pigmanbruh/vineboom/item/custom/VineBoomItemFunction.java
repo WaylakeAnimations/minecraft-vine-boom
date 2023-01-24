f96f5c25617b01439af070fe92964c7dc3f9d7d0
PigmanBruh <pigmanbruh4@gmail.com>
2023-01-24 17:16:47 +0700

    fixing errors

--- src/main/java/net/pigmanbruh/vineboom/item/custom/VineBoomItemFunction.java
+++ src/main/java/net/pigmanbruh/vineboom/item/custom/VineBoomItemFunction.java
@@ -26,14 +26,14 @@ public class VineBoomItemFunction extends Item {
     public static final Identifier VINE_BOOM_SOUND = new Identifier("vineboom:vine_boom_sound");
     public static SoundEvent VINE_BOOM_SOUND_EVENT = SoundEvent.of(VINE_BOOM_SOUND);
 
-    public void registerModSounds() {
+    public static void registerModSounds() {
         Registry.register(Registries.SOUND_EVENT, VineBoomItemFunction.VINE_BOOM_SOUND, VINE_BOOM_SOUND_EVENT);
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient() && hand == Hand.MAIN_HAND) {
-            user.playSound(Moditems.VINE_BOOM_SOUND_EVENT, 1.0f, 1.0f);
+            user.playSound(VineBoomItemFunction.VINE_BOOM_SOUND_EVENT, 1.0f, 1.0f);
             user.getItemCooldownManager().set(this, 20);
         }
         return super.use(world, user, hand);
