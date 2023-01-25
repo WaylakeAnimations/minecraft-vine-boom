2a1ddca7e76039c91ca365b5003607d8f5b0617b
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-25 09:47:37 +0700

    Update VineBoomItemFunction.java

--- src/main/java/net/pigmanbruh/vineboom/item/custom/VineBoomItemFunction.java
+++ src/main/java/net/pigmanbruh/vineboom/item/custom/VineBoomItemFunction.java
@@ -15,7 +15,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.entity.EntityType<T>;
 
 public class VineBoomItemFunction extends Item {
     public VineBoomItemFunction(Settings settings) {
@@ -32,7 +32,7 @@ public class VineBoomItemFunction extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient() && hand == Hand.MAIN_HAND) {
-            CommandManager.executeWithPrefix(ServerCommandSource.entity, "playsound vineboom:vine_boom_sound player @a ~ ~ ~ 1.0 1.0");
+            CommandManager.executeWithPrefix(EntityType.PLAYER, "playsound vineboom:vine_boom_sound player @a ~ ~ ~ 1.0 1.0");
             user.getItemCooldownManager().set(this, 10);
         }
         return super.use(world, user, hand);
