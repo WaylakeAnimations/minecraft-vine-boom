c0c2d593aff03186ad89002685e9fefc50d1de42
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-25 20:00:38 +0700

    Update Moditems.java

--- src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
+++ src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
@@ -1,4 +1,6 @@
 package net.pigmanbruh.vineboom.item;
+// This class is used for adding and registering mod item(s)
+// and also adding item into an item group
 
 import net.pigmanbruh.vineboom.item.custom.VineBoomItemFunction;
 import net.pigmanbruh.vineboom.Main;
@@ -6,13 +8,10 @@ import net.pigmanbruh.vineboom.Main;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
-import net.minecraft.item.ItemStack;
 
 public class Moditems {
 
@@ -22,10 +21,6 @@ public class Moditems {
 
     public static void addItemsToItemGroups() {
         addToItemGroup(ItemGroups.TOOLS, VINE_BOOM_ITEM);
-    }
-    
-    private static void addToItemGroup(ItemGroup group, Item item) {
-        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
     }
 
     public static void registerModItems() {
