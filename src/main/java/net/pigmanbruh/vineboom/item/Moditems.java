ca7f65e2a077897bd76ebcbfbfbcd446c49f7608
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-01-25 20:09:59 +0700

    fixing cannot find symbol

--- src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
+++ src/main/java/net/pigmanbruh/vineboom/item/Moditems.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
